db1c177ba637f58bb11feddf9e8e82244234c0b0
Zoltan Kepes <zoltan.the.kepes@mail.com>
2023-10-24 23:31:46 +0100

    footer with correct links

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,44 +1,59 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
 
 const FeatureList = [
   {
-    title: 'Powered by "Docusaurus"',
-    Svg: require('@site/static/img/elements/logo_original.svg').default,
+    title: "Build with Docusaurus",
+    Svg: require("@site/static/img/elements/logo_original.svg").default,
     description: (
       <>
-        The website is built with <a href='https://docusaurus.io/' target="_blank">Docusaurus</a>, 
-        which is a fantastic tool for building sites from Markdown.
+        The website is built with{" "}
+        <a href="https://docusaurus.io/" target="_blank">
+          Docusaurus
+        </a>
+        , which is a fantastic tool for building sites from Markdown.
       </>
     ),
   },
   {
-    title: 'Download Notes from GitHub',
-    Svg: require('@site/static/img/elements/markdown.svg').default,
+    title: "Download Notes from GitHub",
+    Svg: require("@site/static/img/elements/markdown.svg").default,
     description: (
       <>
-      Every page has a link &nbsp;
-      <svg fill="currentColor" height="20" width="20" viewBox="0 0 40 40" color='#2e8555' ><g><path d="m34.5 11.7l-3 3.1-6.3-6.3 3.1-3q0.5-0.5 1.2-0.5t1.1 0.5l3.9 3.9q0.5 0.4 0.5 1.1t-0.5 1.2z m-29.5 17.1l18.4-18.5 6.3 6.3-18.4 18.4h-6.3v-6.2z"></path></g></svg>
-      &nbsp; that will take you to my GitHub page. From there you can download any note as Markdown for free.
+        Every page has a link &nbsp;
+        <svg
+          fill="currentColor"
+          height="25"
+          width="25"
+          viewBox="0 0 25 25"
+          color="#2e8555"
+        >
+          <g>
+            <path d="M12 0a12 12 0 1 0 0 24 12 12 0 0 0 0-24zm3.163 21.783h-.093a.513.513 0 0 1-.382-.14.513.513 0 0 1-.14-.372v-1.406c.006-.467.01-.94.01-1.416a3.693 3.693 0 0 0-.151-1.028 1.832 1.832 0 0 0-.542-.875 8.014 8.014 0 0 0 2.038-.471 4.051 4.051 0 0 0 1.466-.964c.407-.427.71-.943.885-1.506a6.77 6.77 0 0 0 .3-2.13 4.138 4.138 0 0 0-.26-1.476 3.892 3.892 0 0 0-.795-1.284 2.81 2.81 0 0 0 .162-.582c.033-.2.05-.402.05-.604 0-.26-.03-.52-.09-.773a5.309 5.309 0 0 0-.221-.763.293.293 0 0 0-.111-.02h-.11c-.23.002-.456.04-.674.111a5.34 5.34 0 0 0-.703.26 6.503 6.503 0 0 0-.661.343c-.215.127-.405.249-.573.362a9.578 9.578 0 0 0-5.143 0 13.507 13.507 0 0 0-.572-.362 6.022 6.022 0 0 0-.672-.342 4.516 4.516 0 0 0-.705-.261 2.203 2.203 0 0 0-.662-.111h-.11a.29.29 0 0 0-.11.02 5.844 5.844 0 0 0-.23.763c-.054.254-.08.513-.081.773 0 .202.017.404.051.604.033.199.086.394.16.582A3.888 3.888 0 0 0 5.702 10a4.142 4.142 0 0 0-.263 1.476 6.871 6.871 0 0 0 .292 2.12c.181.563.483 1.08.884 1.516.415.422.915.75 1.466.964.653.25 1.337.41 2.033.476a1.828 1.828 0 0 0-.452.633 2.99 2.99 0 0 0-.2.744 2.754 2.754 0 0 1-1.175.27 1.788 1.788 0 0 1-1.065-.3 2.904 2.904 0 0 1-.752-.824 3.1 3.1 0 0 0-.292-.382 2.693 2.693 0 0 0-.372-.343 1.841 1.841 0 0 0-.432-.24 1.2 1.2 0 0 0-.481-.101c-.04.001-.08.005-.12.01a.649.649 0 0 0-.162.02.408.408 0 0 0-.13.06.116.116 0 0 0-.06.1.33.33 0 0 0 .14.242c.093.074.17.131.232.171l.03.021c.133.103.261.214.382.333.112.098.213.209.3.33.09.119.168.246.231.381.073.134.15.288.231.463.188.474.522.875.954 1.145.453.243.961.364 1.476.351.174 0 .349-.01.522-.03.172-.028.343-.057.515-.091v1.743a.5.5 0 0 1-.533.521h-.062a10.286 10.286 0 1 1 6.324 0v.005z"></path>
+          </g>
+        </svg>
+        &nbsp; that will take you to my GitHub page. From there you can download
+        any note as Markdown for free.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Search with Algolia",
+    Svg: require("@site/static/img/elements/Algolia_Robust_Website_Search.svg")
+      .default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        DocSearch crawls the site and aggregates all the content in an Algolia
+        index. This content is then queried directly through the search box.
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1>
-        <img
-        src={require('../../static/img/elements/notes.zoltankepes_notes_logo_title.png').default}
-  alt="page title 'notes zoltankepes.com'"
-/>
-</h1>
+          <img
+            src={
+              require("../../static/img/elements/notes.zoltankepes_notes_logo_title.png")
+                .default
+            }
+            alt="page title 'notes zoltankepes.com'"
+          />
+        </h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
       </div>
     </header>
@@ -25,12 +28,13 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       // title={`Hello from ${siteConfig.title}`}
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Collection of Zoltan's programming notes."
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
